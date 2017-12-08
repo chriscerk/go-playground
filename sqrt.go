fe3e5587f15da722cc451f1b3ca775bc3123db76
Chris Cerk <chrcerk@umich.edu>
2017-12-08 00:56:15 -0500

    defer, seed rand, and comparing results

--- sqrt.go
+++ sqrt.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
+	"math/rand"
 )
 
 func Sqrt(x float64) float64 {		
@@ -17,7 +19,21 @@ func SqrtHelper(x, z, i float64) float64 {
 	return SqrtHelper(x, z-((z*z - x) / (2*z)), i+1)
 }
 
+func CompareResults(q, mySqrt, mathSqrt float64) {
+	defer fmt.Println("\n")
+	defer fmt.Printf("\n math.Sqrt: %d", mathSqrt)
+	defer fmt.Printf("\n My Sqrt:   %d", mySqrt)
+
+	fmt.Printf("\n For the Square Root of %s", int(q))
+	fmt.Printf("\n Difference Between: %d", math.Abs(mySqrt - mathSqrt))
+} 
+
 func main() {
-	q := 99.0
-	fmt.Println(Sqrt(q))
-	fmt.Println(math.Sqrt(q))
+	rand.Seed(time.Now().UnixNano())
+
+	q := float64(rand.Intn(10000))
+	mySqrt := Sqrt(q)
+	mathSqrt := math.Sqrt(q)
+
+	CompareResults(q, mySqrt, mathSqrt)
+}
