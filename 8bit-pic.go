e130211165a5c149d1f4489194b8d0cd13dc9daf
Chris Cerk <chrcerk@umich.edu>
2017-12-08 01:35:05 -0500

    8bit pic reformat

--- 8bit-pic.go
+++ 8bit-pic.go
@@ -5,17 +5,17 @@ import "math"
 
 func Pic(dx, dy int) [][]uint8 {
 	ss := make([][]uint8, dy)
-    for i := range ss {
-        ss[i] = make([]uint8, dx)
-    }
-	
-	for i := 0; i < dy; i++ {
-        ss[i] = make([]uint8, dx)
-        for j := 0; j < dx; j++ {
-            ss[i][j] = uint8(math.Abs(float64(i^j)-float64(j*2)))
-        }
+	for i := range ss {
+		ss[i] = make([]uint8, dx)
 	}
-	
+
+	for i := 0; i < dy; i++ {
+		ss[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			ss[i][j] = uint8(math.Abs(float64(i^j)-float64(j*2)))
+		}
+	}
+
 	return a
 }
 
