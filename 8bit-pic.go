c90f4585edf3b91ef2c91ba7a263d974a5fc88ce
Chris Cerk <chrcerk@umich.edu>
2017-12-08 01:31:05 -0500

    8bit pic

---
+++ 8bit-pic.go
@@ -0,0 +1,25 @@
+package main
+
+import "golang.org/x/tour/pic"
+import "math"
+
+func Pic(dx, dy int) [][]uint8 {
+	
+	a := make([][]uint8, dy)
+    for i := range a {
+        a[i] = make([]uint8, dx)
+    }
+	
+	for i := 0; i < dy; i++ {
+        a[i] = make([]uint8, dx)
+        for j := 0; j < dx; j++ {
+            a[i][j] = uint8(math.Abs(float64(i^j)-float64(j*2)))
+        }
+    }
+	
+	return a
+}
+
+func main() {
+	pic.Show(Pic)
+}
