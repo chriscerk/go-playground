93df2f77ea5247f21c0ce319a5a6b8720502a496
Chris Cerk <chrcerk@umich.edu>
2017-12-07 23:59:05 -0500

    initial commit

---
+++ sqrt.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {		
+	return SqrtHelper(x, 1.0, 1.0)
+}
+
+func SqrtHelper(x float64, z float64, i float64) float64 {
+	if i > x*10 {
+		return z
+	}
+		
+	return SqrtHelper(x, z-((z*z - x) / (2*z)), i+1)
+}
+
+
+func main() {
+	q := 99.0
+	fmt.Println(Sqrt(q))
+	fmt.Println(math.Sqrt(q))
